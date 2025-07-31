f10f934a4d965890165b01e9f7e979adf5fa62ff
Elaine Ng <128241985+elainengg@users.noreply.github.com>
2025-07-31 13:32:08 -0400

    Remove radio research/market buttons, changed email

--- sidebar.py
+++ sidebar.py
@@ -6,7 +6,6 @@ def sidebar_chat_manager():
         # Top of the Screen
         st.header("Front Office Assistant")
         st.write("### Recent Chats")
-        st.write("#### Older")
 
         # Defaults to just having chat 1
         if "chats" not in st.session_state:
@@ -18,9 +17,11 @@ def sidebar_chat_manager():
             new_name = f"Chat {len(st.session_state.chats) + 1}"
             st.session_state.chats[new_name] = []
             st.session_state.current_chat = new_name
+        
+        st.write("#### Older")
 
         chat_names = list(st.session_state.chats.keys())
-        selected_chat = st.radio("Select a chat:", chat_names, index=chat_names.index(st.session_state.current_chat))
+        selected_chat = st.radio("", chat_names, index=chat_names.index(st.session_state.current_chat))
 
         if selected_chat != st.session_state.current_chat:
             st.session_state.current_chat = selected_chat
